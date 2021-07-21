816db229e7bb6b6ab186ab66c04524c9cc870aca
Jatinokov <Enkhtaivan1206@gmail.com>
2021-07-20 23:56:19 -0700

    to clear the user input and relocate the crusor to the fist input

--- new.js
+++ new.js
@@ -6,7 +6,9 @@ var uiController = (function () {
         add_type: ".add__type",
         add__description: ".add__description",
         add__value: ".add__value",
-        addBTN: ".add__btn"
+        addBTN: ".add__btn",
+        income_list: ".income__list",
+        expense_list: ".expenses__list"
     };
     // PUBLIC SERVICE OF ui-CONTROLLER 
     return {
@@ -18,6 +20,41 @@ var uiController = (function () {
                 // btn: document.querySelector(domClasses.addBTN)
             }
         },
+        // public function for DOM
+        addList_items: function (type, items) {
+            var dom, list;
+            if (type === "inc") {
+                list = domClasses.income_list;
+                dom = '<div class="item clearfix" id="income-$$ID$$"><div class="item__description">$$DESCRIPTION$$</div><div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+
+            } else {
+                list = domClasses.expense_list;
+                dom = '<div class="item clearfix" id="expense-$$ID$$"><div class="item__description">$$DESCRIPTION$$</div><div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+            }
+            dom = dom.replace("$$ID$$", items.id);
+            dom = dom.replace("$$DESCRIPTION$$", items.description);
+            dom = dom.replace("$$VALUE$$", items.value);
+
+            document.querySelector(list).insertAdjacentHTML("beforeend", dom);
+        },
+        // clearing input // resset input 
+        input_clear: function () {
+            var clearfiled = document.querySelectorAll(domClasses.add__description + "," + domClasses.add__value);
+            // convert it into array
+            var newArray_input = Array.prototype.slice.call(clearfiled);
+            // for(var i =0; i<newArray_input.length; i++){
+            //     newArray_input[0] = "";
+            // }
+            // same as for loop
+            newArray_input.forEach(function (el) {
+                // to give empty to the arrays each element
+                el.value = "";
+            });
+            // after enter the data users crusor focus on first input
+            newArray_input[0].focus()
+
+        },
+
         addButton: function () {
             return domClasses;
         }
@@ -59,12 +96,30 @@ var finController = (function () {
 
     // all_User_Data.data.inc_Data.push(i1)
     // all_User_Data.data.exp_Data.push(e1)
-    console.log(all_User_Data);
+    // console.log(all_User_Data);
     // PUBKIC SERVICE FUNCTION OF FINCANCE CONTROLLER
     return {
         addData: function (type, desc, val) {
-            all_User_Data.data[type].push(id, desc, val)
-        }
+            var items, id;
+            if (all_User_Data.data[type].length === 0) {
+                id = 1;
+            } else {
+                // got lastest item from array and add new item after it then put their id's in id
+                id = all_User_Data.data[type][all_User_Data.data[type].length - 1].id + 1;
+            };
+
+            if (type === "inc") {
+                items = new Income(id, desc, val);
+            } else {
+                items = new Expense(id, desc, val);
+            }
+            all_User_Data.data[type].push(items);
+
+            return items;
+        },
+        // see:function(){
+        //     return all_User_Data;
+        // }
     }
 
 
@@ -77,11 +132,14 @@ var conController = (function (ui, fin) {
     var enterfunction = function () {
         // get input values from ui
         var d = ui.uiPublic();
-        // console.log(x.description, x.value, x.type);
+        // console.log(d.description, d.value, d.type);
         // pass & save the value to finController
-        var ff = fin.addData(d.type, d.description, d.value)
-        console.log(ff);
+        var returned_Item = fin.addData(d.type, d.description, d.value)
+        // console.log(returned_Item.id);
         // value to DOM
+        ui.addList_items(d.type, returned_Item);
+        // clear the input field and focus the cursor in the first input
+        ui.input_clear()
         // calculate fincance
         // final result to DOM
 
